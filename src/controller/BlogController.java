a1ea8f8efa2e508c03338e4b2fa2a8033a235e43
saiteja8848 <saiteja8848@gmail.com>
2020-05-14 22:28:49 +0530

    FSJ0420A102

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -12,24 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Blog;
 import model.User;
+import utility.CheckBlogPost;
 
-
-@WebServlet(urlPatterns= {"/blog"})
+@WebServlet(urlPatterns = { "/blog" })
 public class BlogController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public BlogController() {
-        super();
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
-		rd.forward(request, response);
-		
+	public BlogController() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
+		rd.forward(request, response);
+
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String blogDetails = request.getParameter("selectedAnswers")	;
@@ -48,21 +46,23 @@ public class BlogController extends HttpServlet {
 		blog.setBlogDescription(description);
 		blog.setDate(postedOn);
 
-		if(checkblog!) {
-			request.setAttribute("blog", blog);
-			request.setAttribute("user",user);
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
-			rd.forward(request, response);
-		}
-		else{
-			
-			request.setAttribute("error", "Your blog cannot be added as it contains offensive words, Please check your blog");
-
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
-			rd.forward(request, response);
-			
-			
-		}
+		CheckBlogPost checkBlogPost = new CheckBlogPost();
+		boolean checkblog=checkBlogPost.checkBlog(blog);
+		
+		System.out.println("blog status:"+checkblog);
+		
+		  if(checkblog) { 
+			  request.setAttribute("blog", blog);
+		       request.setAttribute("user",user); 
+		       RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp"); rd.forward(request, response); } else{
+		  request.setAttribute("error","Your blog cannot be added as it contains offensive words, Please check your blog");
+		  
+		  RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
+		  "/WEB-INF/views/blogView.jsp"); rd.forward(request, response);
+		  
+		  
+		  }
+		 
 		
 	}
 
--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,63 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface {
+
+	String[] offensiveWords = {
+			"chocolate rosebuds",
+			"chode",
+			"chodes",
+			"chota bags",
+			"chota bags",
+			"cipa",
+			"circlejerk",
+			"cl1t",
+			"cleveland steamer",
+			"climax",
+			"damn",
+			"damned",
+			"damnit",
+			"darkie",
+			"darn"};
+	
+	
+	public boolean checkBlogTitle(Blog blog) {
+		
+		boolean status=false;
+		String[] blogData = blog.getBlogTitle().split(" ");
+		for(int i=0;i<blogData.length;i++) {
+			for(int j=0;j<offensiveWords.length;j++)
+			if(blogData[i].equalsIgnoreCase(offensiveWords[j])) {
+				status=true;
+				break;
+			}
+		}
+		return status;
+	}
+
+	
+	public boolean checkBlogDescription(Blog blog) {
+	
+		boolean status=false;
+		String[] blogData = blog.getBlogDescription().split(" ");
+		for(int i=0;i<blogData.length;i++) {
+			for(int j=0;j<offensiveWords.length;j++)
+			if(blogData[i].equalsIgnoreCase(offensiveWords[j])) {
+				status=true;
+				break;
+			}
+		}
+		return status;		
+
+	}
+
+	
+	public boolean checkBlog(Blog blog) {
+		if(checkBlogTitle(blog)&&checkBlogDescription(blog))
+			return true;
+		else	
+		   return false;
+	}
+	
+}
